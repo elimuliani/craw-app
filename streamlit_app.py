b134ed17f336f5e3a9a79dc1621e149ff47ba904
elimuliani <137256683+elimuliani@users.noreply.github.com>
2024-10-21 10:14:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Membaca dataset yang sudah dibersihkan
+data = pd.read_csv('data_with_sentiment.csv')
+
+# Judul aplikasi web
+st.title("Analisis PESTEL untuk PT PLN")
+
+# Tampilkan dataset
+st.write("Tabel Data Analisis:")
+st.dataframe(data)
+
+# Buat grafik distribusi kategori PESTEL
+st.write("Grafik Distribusi Kategori PESTEL")
+plt.figure(figsize=(8, 6))
+sns.countplot(x='PESTEL_category', data=data, palette='viridis')
+plt.title('Distribusi Kategori PESTEL')
+st.pyplot(plt)
+
+# Buat grafik distribusi sentimen
+st.write("Grafik Distribusi Sentimen")
+plt.figure(figsize=(8, 6))
+sns.countplot(x='Sentiment', data=data, palette='coolwarm')
+plt.title('Distribusi Sentimen Secara Keseluruhan')
+st.pyplot(plt)
+
